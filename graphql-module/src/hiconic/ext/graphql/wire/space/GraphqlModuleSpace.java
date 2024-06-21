efa207dae8f6a8fea659510142c79906ce097af1
Peter Gazdik <peter.gazdik@gmail.com>
2024-06-21 15:00:48 +0200

    removing by-prototype BS

--- graphql-module/src/hiconic/ext/graphql/wire/space/GraphqlModuleSpace.java
+++ graphql-module/src/hiconic/ext/graphql/wire/space/GraphqlModuleSpace.java
@@ -7,20 +7,17 @@ import com.braintribe.model.processing.service.api.ServiceAroundProcessor;
 import com.braintribe.wire.api.annotation.Import;
 import com.braintribe.wire.api.annotation.Managed;
 
-import hiconic.ext.graphql.GraphQlByPrototypeMarshaller;
 import hiconic.ext.graphql.GraphQlRequestMarshaller;
+import hiconic.ext.graphql.api.model.GraphQlRequest;
 import hiconic.ext.graphql.deployment.model.GraphQlAroundProcessor;
 import hiconic.ext.graphql.deployment.model.GraphQlMarshaller;
-import tribefire.module.api.InitializerBindingBuilder;
-import tribefire.module.api.WireContractBindingBuilder;
 import tribefire.module.wire.contract.TribefireModuleContract;
 import tribefire.module.wire.contract.TribefireWebPlatformContract;
 
 /**
- * Bind the GraphQL extension into a platform. It registers a marshaller using the MIME type "graphql". The provided
- * marshaller can transpose a QueryByPrototype request into GraphQL syntax.
- * 
- * QueryByPrototyp offers (only) a very basic functionality of GraphQl but it does it with full type and name safety.
+ * Registers a {@link GraphQlMarshaller} for MIME type "application/graphql".
+ * <p>
+ * This marshaller encodes {@link GraphQlRequest} into GraphQL syntax.
  */
 @Managed
 public class GraphqlModuleSpace implements TribefireModuleContract {
@@ -29,43 +26,13 @@ public class GraphqlModuleSpace implements TribefireModuleContract {
 	private TribefireWebPlatformContract tfPlatform;
 
 	//
-	// WireContracts
-	//
-
-	@Override
-	public void bindWireContracts(WireContractBindingBuilder bindings) {
-		// Bind wire contracts to make them available for other modules.
-		// Note that the Contract class cannot be defined in this module, but must be in
-		// a gm-api artifact.
-	}
-
-	//
 	// Hardwired deployables
 	//
 
 	@Override
 	public void bindHardwired() {
-		String name = "graphql-by-prototype";
-		String externalId = "";
-		tfPlatform.hardwiredDeployables().bindMarshaller(externalId, name, graphQlPrototypeMarshaller(), "graphql-by-prototype");
-
-		tfPlatform.hardwiredDeployables().bindMarshaller("application/graphql.marshaller", "GraphQL Marshaller", graphQlMarshaller(),
-				"application/graphql");
-
-	}
-
-	@Managed
-	private Marshaller graphQlPrototypeMarshaller() {
-		return new GraphQlByPrototypeMarshaller();
-	}
-
-	//
-	// Initializers
-	//
-
-	@Override
-	public void bindInitializers(InitializerBindingBuilder bindings) {
-		// Bind DataInitialiers for various CollaborativeAcceses
+		tfPlatform.hardwiredDeployables().bindMarshaller( //
+				"marshaller.graphql", "GraphQL Marshaller", graphQlMarshaller(), "application/graphql");
 	}
 
 	//
